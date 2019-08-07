184079cfcf4e5a4ec20562fba794fd763ceabfe0
George Monsour <gnmonsour@gmail.com>
2019-08-07 17:16:56 -0400

    basics, authors routing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const mainRouter = require('./routes/index');
 const authorsRouter = require('./routes/authors');
@@ -14,6 +15,10 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: false
+}));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author');
 
 // all authors
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    // searchCriteria
+    let searchCondition = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchCondition.name = new RegExp(req.query.name, 'i');
+    }
+    try {
+        const authors = await Author.find(searchCondition);
+        res.render('authors/index', {
+            authors,
+            searchCondition: req.query
+        });
+    } catch (error) {
+        res.redirect('/');
+    }
 });
 
 // form for new author
 router.get('/new', (req, res) => {
-    res.render('authors/new');
+    res.render('authors/new', {
+        author: new Author()
+    });
 });
 
 // create author
-router.post('/', (req, res) => {
-    res.send('Created author')
-})
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    });
+    try {
+        const candidate = await author.save();
+        // res.redirect(`authors/${candidate.id}`);
+        res.redirect('authors/');
+    } catch (error) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'E R R O R /creating/author'
+        });
+    }
+});
 
 module.exports = router;
