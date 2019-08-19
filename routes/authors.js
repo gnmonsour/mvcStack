25110fa6bef14ac7d2ca8e45fbc2f4e00fbbc639
George Monsour <gnmonsour@gmail.com>
2019-08-19 08:41:30 -0400

    author routes, page,  method-override, pre-remove

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // all authors
 router.get('/', async (req, res) => {
@@ -25,6 +26,69 @@ router.get('/new', (req, res) => {
     res.render('authors/new', {
         author: new Author()
     });
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        const books = await Book.find({
+            author: author.id
+        }).limit(6).exec();
+        author.bookList = books;
+        res.render('authors/showCard', {
+            author
+        });
+    } catch (error) {
+        res.redirect('/authors');
+
+    }
+});
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        res.render('authors/edit', {
+            author,
+        });
+    } catch (error) {
+        res.redirect('/authors');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    // res.send('Delete Author Card' + req.params.id);
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        await author.remove();
+        // res.redirect(`authors/${candidate.id}`);
+        res.redirect(`/authors`);
+    } catch (error) {
+        if (author == null) {
+            return res.status(400).redirect('/');
+        }
+        res.redirect(`/authors/${author.id }`);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    // res.send('Update Author Card' + req.params.id);
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        author.name = req.body.name;
+        await author.save();
+        // res.redirect(`authors/${candidate.id}`);
+        res.redirect(`/authors/${author.id}`);
+    } catch (error) {
+        if (author == null) {
+            return res.redirect('/');
+        }
+        res.render('authors/edit', {
+            author: author,
+            errorMessage: 'E R R O R /updating/author'
+        });
+    }
 });
 
 // create author
